14eb698f8d2e6ff7faed714aa48b8573e3e6bbd1
RealSakifAbdullah <125168225+RealSakifAbdullah@users.noreply.github.com>
2023-02-12 22:37:10 +1100

    Add tires

---
+++ tires/models/octoprime_tires.py
@@ -0,0 +1,13 @@
+from tires.tires_base import Tires
+from datetime import datetime
+
+
+class OctoprimeTires(Tires):
+    def __init__(self, tire_wear):
+        self.tire_wear = tire_wear
+
+    def needs_service(self):
+        sum = 0
+        for tire in self.tire_wear:
+            sum += tire
+        return sum >= 3
