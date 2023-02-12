76e14fa66e7d6b5ceae558ebb2afc80ecf0c26d5
RealSakifAbdullah <125168225+RealSakifAbdullah@users.noreply.github.com>
2023-02-12 22:54:52 +1100

    Added tires to car base

--- car/car_base.py
+++ car/car_base.py
@@ -1,17 +1,12 @@
 from abc import abstractmethod
 from car.serviceable import Serviceable
-from engine.models.capulet_engine import CapuletEngine
-from engine.models.sternman_engine import SternmanEngine
-from engine.models.willoughby_engine import WilloughbyEngine
-from battery.models.nubbin_battery import NubbinBattery
-from battery.models.spindler_battery import SpindlerBattery
-from datetime import datetime
 
 
 class Car(Serviceable):
-    def __init__(self, engine, battery):
+    def __init__(self, engine, battery, tires):
         self.engine = engine
         self.battery = battery
-    
+        self.tires = tires
+
     def needs_service(self):
-        return self.engine.needs_service() or self.battery.needs_service()
+        return self.engine.needs_service() or self.battery.needs_service() or self.tires.needs_service()
